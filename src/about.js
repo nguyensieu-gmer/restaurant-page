ad4903ca179a196bcfe16d44386e916b5babbaa3
Nguyen Van Sieu <217349416+nguyensieu-gmer@users.noreply.github.com>
2026-04-08 05:48:33 +0700

    Add about

--- src/about.js
+++ src/about.js
@@ -1,24 +1,46 @@
 import aboutImage from "./image/about_image.avif";
+import "./about.css";
+import employee1 from "./image/employee.avif";
+import employee2 from "./image/employee1.jpg";
+import employee3 from "./image/employee2.jpg";
 
 export function makeAbout(){
+    const employees = [employee3, employee1, employee2];
+
     const content = document.getElementById("content");
 
-    const title = document.createElement("h1");
-    title.textContent = "About Us";
-    content.appendChild(title);
-
-    const description = document.createElement("h2");
-    description.textContent = "We specialize in [type of cuisine], bringing you carefully crafted dishes made from fresh, high-quality ingredients. Our passion for food comes from [your story – family tradition, love of cooking, cultural inspiration, etc.].";
-    content.appendChild(description);
-
-    const imageContainer = document.createElement("div");
     const image = document.createElement("img");
     image.src = aboutImage;
-    image.alt = "about image";
-    imageContainer.appendChild(image);
-    content.appendChild(imageContainer);
+    image.style.height = "auto";
+    image.style.width = "100%";
+    image.style.zIndex = "-1";
+    image.style.position = "absolute";
+    image.style.top = "0";
+    image.style.filter = "brightness(80%)";
+    content.appendChild(image);
 
-    const paragraph = document.createElement("p");
-    paragraph.textContent = "At [Restaurant Name], we believe that dining is more than just eating — it’s an experience. Whether you're here for a quick meal or a special occasion, we aim to make every visit memorable.Join us and enjoy flavors that feel like home.";
-    content.appendChild(paragraph);
+    const h1 = document.createElement("h1");
+    h1.textContent = "About Us";
+    h1.classList.add("h1_title");
+    content.appendChild(h1);
+
+    const flex_div = document.createElement("div");
+    flex_div.classList.add("flex_div");
+    for (let employee of employees){
+        const item = document.createElement("div");
+        item.classList.add("item");
+        const div = document.createElement("div");
+        const image = document.createElement("img");
+        image.src = employee;
+        const h1 = document.createElement("h1");
+        h1.textContent = "Employee";
+        const p = document.createElement("p");
+        p.textContent = "lorem inspirum";
+        div.appendChild(image);
+        item.appendChild(div);
+        item.appendChild(h1);
+        item.appendChild(p);
+        flex_div.appendChild(item);
+    }
+    content.appendChild(flex_div);
 }
